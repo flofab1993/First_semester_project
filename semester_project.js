ad03127267a612341625b22d5253815f08fd0ebb
flofab1993 <43214718+flofab1993@users.noreply.github.com>
2018-09-24 09:19:10 +0200

    Add files via upload

---
+++ semester_project.js
@@ -0,0 +1,24 @@
+
+submit.onclick = function(){
+
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "flo" && password == "bremen"){
+alert ("Login successfully");
+window.location = "semester_project.html"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+//document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
