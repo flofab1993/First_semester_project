6f2924151a5c2ce86f14bd8a2a918d24ff7dd7ad
ferdinandkff <43066451+ferdinandkff@users.noreply.github.com>
2018-09-27 13:54:44 +0200

    Update semester_project.js

--- semester_project.js
+++ semester_project.js
@@ -11,7 +11,13 @@ submit.onclick = function(){
     console.log(attempt);
     console.log(errorAttempt);
 
-
+    if (attempt >= errorAttempt) { //Shuts down after 3 wrong attempts
+        alert("You are fucked");
+        document.getElementById("username").disabled = true;
+        document.getElementById("password").disabled = true;    
+        return false;
+    }
+    
     if ((username == "flo" && password == "bremen") || (username == "ferdinand" && password == "hamburg") || (username == "malte" && password == "köln") ) { //Opens site with right combination
         alert ("Login successful");
         window.location.href = "http://transfermarkt.de"; // Redirecting to other page.
@@ -20,17 +26,9 @@ submit.onclick = function(){
     } else if (errorAttempt - attempt == 1) { //Alert when only one attempt is left
         alert("You have " + (errorAttempt - attempt) + " attempt left");
         attempt++;
-
     } else { //Alert when more then one attempt is left
         alert("You have " + (errorAttempt - attempt) + " attempts left");
         attempt++;
-    }
-
-    if (attempt > errorAttempt) { //Shuts down after 3 wrong attempts
-        alert("You are fucked");
-        document.getElementById("username").disabled = true;
-        document.getElementById("password").disabled = true;    
-        return false;
     }
 }
 
