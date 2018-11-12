d5ea78919d38f3279ced56b9d951ae021b0aed66
ferdinandkff <ferdinand.krumhoff@yahoo.de>
2018-11-12 09:30:50 +0100

    User can now only take the survey once. If the login after they have taken the survey, they will be redirected to the pie charts directly. The class is now called 'Charts' and can take questions with any number of possible answers. That means it works both for Genders and for Nationalities. It is also possible to make bar charts through that class.

--- survey.js
+++ survey.js
@@ -1,46 +1,117 @@
 function signUp() { // New user registration
-  
+
+  // Create Users array, if it does not exist already
+  if(localStorage.getItem("Users") === null) {
+    localStorage.setItem("Users", JSON.stringify([])); // Creates empty array named "Users"
+  }
+
   // Read user input
   let username = document.getElementById("newUser").value;
   let password = document.getElementById("newPassword").value;
   let confirmPw = document.getElementById("confirmPassword").value;
 
   var emailValid = /\w{4}1\d\w{2}@student.cbs.dk/; // Regular expression for a CBS mail adress no older than from 2010
+  var userArr = JSON.parse(localStorage.getItem("Users")) // Array containing login data of every user as an object
+
+  // Check if user already exists
+  let userOk = true 
+
+  for (let i =0; i<userArr.length; i++) {
+    if (userArr[i].username === username) {
+      userOk = false; // If username exists, userOk becomes false (Basically: user is not ok)
+      break; // Break out of the loop
+    }
+  }
 
   // Validate Input
   if(!emailValid.test(username)) { // Check if username is a CBS mail adress
     alert("Username must be a CBS email adress!");
     return false;
-  } else if (localStorage.getItem(username) != null) { // Check if user already exists
+  } else if (!userOk) { // Check if user already exists
     alert("User already exists!");
     return false;
   } else if (password != confirmPw) { // Check if password confirmation matches
     alert("Your passwords don't match!");
     return false;
-  } else {
-    localStorage.setItem(username, password); // Add user to local storage
+  } else { 
+    userArr.push({ // Push an object with the user's username and password to userArr
+      username: username,
+      password: password
+    })
+    localStorage.setItem("Users", JSON.stringify(userArr)) // Load the new userArr into the local storage
+    alert("You have been successfully registered. You will now be redirected to the login page")
   }
 }
 
 function signIn() { // Login function
   
   // Read user input
-  let username = document.getElementById("username").value;
-  let password = document.getElementById("password").value;
+  let loginName = document.getElementById("username").value;
+  let loginPassword = document.getElementById("password").value;
+  
+  let userArr = JSON.parse(localStorage.getItem("Users")) // Get array with all users from local storage
 
-  if (localStorage.getItem(username) === null) { // Check if username exists
-    alert("The user doesn't exist")
+  // Validate input
+  let usernameOK = false 
+  let passwordOK = false
+
+  for (let i=0; i<userArr.length; i++) {
+    if (userArr[i].username === loginName && userArr[i].password === loginPassword) { // If a user object exists in local storage, where both username and password match with input
+      usernameOK = true;
+      passwordOK = true;
+      sessionStorage.setItem("Active user",loginName) // To check later, whether the user has already taken the survey, we will temporarily store his name in the session storage
+      break;
+    } else if (userArr[i].username === loginName) { // If only the username matches
+      usernameOK = true;
+    }
+  }
+
+  if (!usernameOK) { // If the username does not exist, this message will be shown
+    alert("User does not exist!");
     return false;
-  } else if (localStorage.getItem(username) != password) { // CHeck if username and password match
+  } else if (!passwordOK) { // If only the username was ok, this message will be shown
     alert("Wrong password!")
     return false;
+  }
+
+  // Redirecting condition: If the user has already taken the survey, login leads directly to the pie charts
+  // If not, he gets to the survey
+  let takenArr = JSON.parse(localStorage.getItem("Survey taken"))
+  let userIsNew = true
+
+  for (let i=0; i<takenArr.length; i++) {
+    if(takenArr[i] === loginName) {
+      userIsNew = false;
+      break;
+    }
+  }
+
+  // If the user has not taken the survey yet (user is new) he will be redirected to the survey
+  if(userIsNew) {
+    window.open('survey.html',"_self"); // "_self" makes the html page replace the current page
+  } else {
+    window.open('pie_charts.html',"_self");
   }
 }
 
 // Count number of students that have taken the survey
 document.getElementById("userCounter").innerHTML = "We have asked " + localStorage.length + " E-Business students.";
 
-function saveInput() { // Save Input in local storage
+function saveInput() { // Save Input upon submit
+
+  // Make sure everyone only takes the survey once
+  if (localStorage.getItem("Survey taken") === null) {
+    localStorage.setItem("Survey taken", JSON.stringify([])) // If array doesn't exist yet, create one
+  }
+
+  // Post the username to the list (array) of users that have already taken the survey
+  var takenArr = JSON.parse(localStorage.getItem("Survey taken")); // Get the exisiting array
+
+  takenArr.push(sessionStorage.getItem("Active user")); // Push the active user to the array
+
+  localStorage.setItem("Survey taken", JSON.stringify(takenArr)) // Put the array back into the local storage
+
+  localStorage.removeItem("Active user"); // Remove the active user from the session storage
 
   // Calculate Age
   // Get the values for day, month and year
@@ -95,97 +166,127 @@ function saveInput() { // Save Input in local storage
 }
 
 google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawPieChart);
-google.charts.setOnLoadCallback(drawCountryChart);
-google.charts.setOnLoadCallback(drawCPHChart);
+google.charts.setOnLoadCallback(drawChart);
 
 // Class to draw charts with up to 5 values
-class OrChoice {
+class Chart {
 
-  constructor(name, id, value1, value2, value3, value4, value5) {
-    this.name = name;
-    this.id = id;
-    this.value1 = value1;
-    this.value2 = value2;
-    this.value3 = value3;
-    this.value4 = value4;
-    this.value5 = value5;
-  }
+    constructor(name, id) {
+        this.name = name; // name of property in result object
+        this.id = id; // id of chart div in html file
+    }
 
-  drawChart() {
-    let valueArr = [];
+    drawPieChart() {
+        let valueArr = []; 
 
-      for (let i=0; i<localStorage.length; i++) {
-        let usr = JSON.parse(localStorage.getItem(localStorage.key(i)));
-        valueArr.push(usr[this.name]);
-      }
-
-      let value1Nr = 0;
-      let value2Nr = 0;
-      let value3Nr = 0;
-      let value4Nr = 0;
-      let value5Nr = 0;
-
-      for (let i=0; i<valueArr.length; i++) {
-
-        switch (valueArr[i]) {
-          case this.value1:
-            value1Nr++
-            break;
-          case this.value2:
-            value2Nr++
-            break;
-          case this.value3:
-            value3Nr++
-            break;
-          case this.value4:
-            value4Nr++
-            break;
-          case this.value5:
-            value5Nr++
-            break;
+        for (let i = 0; i < localStorage.length; i++) { // valueArr gets filled with the answers the subjects gave
+            if (localStorage.key(i) != "Users" && localStorage.key(i) != "Survey taken") {
+                let usr = JSON.parse(localStorage.getItem(localStorage.key(i)));
+                valueArr.push(usr[this.name]);
+            }
         }
-      }
 
-      var data = google.visualization.arrayToDataTable([
-        [this.name, 'Number'],
-        [this.value1, value1Nr],
-        [this.value2, value2Nr],
-        [this.value3, value3Nr],
-        [this.value4, value4Nr],
-        [this.value5, value5Nr]
-      ])
+        // Create object listing the number of times each answer possibility occurs
+        var numberOfValue = {};
 
-      var options = {
-          /* title: 'This is a pie chart:' */
-      };
+        valueArr.forEach(function(n) {  // Creates a property for each answer possibility with its amount as the value
+            numberOfValue[n] = (numberOfValue[n] || 0) + 1; 
+        });
 
-      var chart = new google.visualization.PieChart(document.getElementById(this.id));
+        // 2-Dimensional array with each answer possibility and their number of occurence
+        var rankedValues = [];
+        
+        for(let x in numberOfValue){
+            rankedValues.push([x, numberOfValue[x]]) // x is the name of the answer
+        }
 
-      chart.draw(data, options);
-  }
+        rankedValues.sort(function(a,b){ // Sort array descending by the amount an answer occurs
+            return b[1] - a[1];
+        }); 
+
+        // We just want to display the top 5 recurring answers in the dataset, therefore we will further adjust
+        var otherValues = 0; // This will become the number of answers given that were not part of the top 5
+
+        for(let i=5; i<rankedValues.length; i++) { // Start from 5 and loop through rankedValues and add values to otherValues
+            otherValues += rankedValues[i][1]; // We only want elements from the second column, which are the number of occurences
+        }
+  
+        // Adjusting rankedValues to go into pie chart
+        rankedValues.splice(4,rankedValues.length-5); // Remove every element but the first five
+        rankedValues.unshift(['Name','Number']) // Add labels
+        rankedValues.push(['Other',otherValues]); // Add total of other answers
+    
+        var data = google.visualization.arrayToDataTable(rankedValues); // Make pie chart
+
+        var options = {
+            
+        };
+    
+        var chart = new google.visualization.PieChart(document.getElementById(this.id)); // Reference to pie chart in .html file
+        chart.draw(data, options);   
+    }
+
+    drawBarChart() {
+        let valueArr = []
+
+        for (let i=0; i<localStorage.length; i++) {
+            if (localStorage.key(i) != "Users" && localStorage.key(i) != "Survey taken") {
+                var usr = JSON.parse(localStorage.getItem(localStorage.key(i)));
+                valueArr.push(usr[this.name]);
+            }
+        }
+
+        var numberOfValue = {};
+
+        valueArr.forEach(function(n) {  
+            numberOfValue[n] = (numberOfValue[n] || 0) + 1; 
+        });
+
+        var rankedValues = []; 
+
+        for(let x in numberOfValue){
+            rankedValues.push([x, numberOfValue[x]])
+        }
+
+        rankedValues.sort(function(a,b){
+            return b[1] - a[1];
+        });
+
+        rankedValues.unshift(['Name','Number']) // Add labels, as required by Google Charts
+
+        var data = google.visualization.arrayToDataTable(rankedValues); // Make pie chart
+
+        var options = {
+        };
+    
+        var chart = new google.visualization.BarChart(document.getElementById(this.id)); // Reference to bar chart in .html file
+        chart.draw(data, options);
+    }
 }
-
-// Create instanceses of the OrClass to build Pie Charts
-var genders    = new OrChoice('gender','genderChart','female','male');
-var relStatus  = new OrChoice('status','statusChart','single','taken','married');
-var eyeColor   = new OrChoice('eye_color','eyeChart','brown','blue','green','grey','Other');
-var veggie     = new OrChoice('eating','veggieChart','none','vegetarian','vegan');
-var beerWine   = new OrChoice('drinks','drinksChart','beer','wine');
-var dogCat     = new OrChoice('pet','petChart','dog','cat');
-var pizzaPasta = new OrChoice('food','pizzaChart','pizza','pasta');
-var coffeeTea  = new OrChoice('hotDrink','coffeeChart','coffee','tea');
+// Create instances of the Chart class to build Pie Charts
+var genders    = new Chart('gender','genderChart');
+var countries  = new Chart('nationality','countryChart');
+var districts  = new Chart('district','cphChart');
+var relStatus  = new Chart('status','statusChart');
+var eyeColor   = new Chart('eye_color','eyeChart');
+var veggie     = new Chart('eating','veggieChart');
+var beerWine   = new Chart('drinks','drinksChart');
+var dogCat     = new Chart('pet','petChart');
+var pizzaPasta = new Chart('food','pizzaChart');
+var coffeeTea  = new Chart('hotDrink','coffeeChart');
 
 // Function to load in callback with google's API
-function drawPieChart() {
-    genders.drawChart();
-    relStatus.drawChart();
-    eyeColor.drawChart();
-    veggie.drawChart();
-    beerWine.drawChart();
-    dogCat.drawChart();
-    pizzaPasta.drawChart();
-    coffeeTea.drawChart();
+function drawChart() {
+    genders.drawPieChart();
+    countries.drawPieChart();
+    relStatus.drawPieChart();
+    eyeColor.drawPieChart();
+    veggie.drawPieChart();
+    beerWine.drawPieChart();
+    dogCat.drawPieChart();
+    pizzaPasta.drawPieChart();
+    coffeeTea.drawPieChart();
+    districts.drawBarChart()
 }
 
 // Display average age
@@ -209,88 +310,3 @@ var averageAge = ageSum/ageArr.length
 
 // Display in HTML with the average Age rounded to two decimals
 document.getElementById("avgAge").innerHTML = "The average age of your fellow students is " + Math.round(averageAge * 100)/100 + " years"
-
-// Nationality chart
-function drawCountryChart() {
-  // Create array containing the value for nationality from each user
-  let countryArr = [];
-
-  for (let i = 0; i < localStorage.length; i++) {
-    let usr = JSON.parse(localStorage.getItem(localStorage.key(i)));
-    countryArr.push(usr.nationality);
-  }
-  
-  // Create object listing the number of times each country occurs
-  var numberOfCountry = {};
-
-  countryArr.forEach(function(n) {  // Creates a property for each country with its amount as the value
-    numberOfCountry[n] = (numberOfCountry[n] || 0) + 1; 
-  });
-
-  var rankedCntrs = []; // Create 2-dimensional array from object numberOfCountry
-  for(let x in numberOfCountry){
-    rankedCntrs.push([x, numberOfCountry[x]])
-  }
-
-  rankedCntrs.sort(function(a,b){ // Sort array descending by the amount a country occurs
-    return b[1] - a[1];
-  }); 
-
-  // We just want to display the top 5 recurring countries in the dataset, therefore we will further adjust rankedCntrs
-  var otherCntrs = 0; // Empty variable for the total amount of countries outside the top five
-
-  for(i=5; i<rankedCntrs.length; i++) { // Start from 5 and loop through rankedCntrs and add values to otherCntrs
-    otherCntrs += rankedCntrs[i][1]; // We only want elements from the second column
-  }
-  
-  // Adjusting rankedCntrs to go into pie chart
-  rankedCntrs.splice(4,rankedCntrs.length-5); // Remove every element but the first five
-  rankedCntrs.unshift(['Country','Number']) // Add labels
-  rankedCntrs.push(['Other',otherCntrs]); // Add total of other countries
-  
-  var data = google.visualization.arrayToDataTable(rankedCntrs); // Make pie chart
-
-  var options = { // Pie chart title
-    title: 'This is where your fellow students are from:',
-  };
-  
-  var chart = new google.visualization.PieChart(document.getElementById('countryChart')); // Reference to pie chart in .html file
-  chart.draw(data, options);
-}
-
-// Draw charts for CPH districts
-function drawCPHChart() {
-  
-  // Create array containing the district from each user
-  let cphArr = [];
-
-  for(let i = 0; i < localStorage.length; i++) {
-    let usr = JSON.parse(localStorage.getItem(localStorage.key(i)));
-    cphArr.push(usr.district);
-  }
-  
-  // Create object listing the number of times each district occurs
-  var numberOfDistrict = {};
-
-  cphArr.forEach(function(n) {  // Creates a property for each district with its amount as the value
-    numberOfDistrict[n] = (numberOfDistrict[n] || 0) + 1; 
-  });
-
-  var rankedDstrcts = []; // Create 2-dimensional array from object numberOfDistricts
-  for(let x in numberOfDistrict){
-    rankedDstrcts.push([x, numberOfDistrict[x]])
-  }
-
-  rankedDstrcts.sort(function(a,b){ // Sort array descending by the amount a district occurs
-    return b[1] - a[1];
-  }).unshift(["District", "Number"]); // Add labels
-
-  var data = google.visualization.arrayToDataTable(rankedDstrcts); // Make pie chart of nationalities with the array rankedCntrs
-
-  var options = { // Pie chart title
-    title: 'This is where your fellow students live:'
-  };
-  
-  var chart = new google.visualization.BarChart(document.getElementById('cphChart')); // Reference to pie chart in .html file
-  chart.draw(data, options);
-}
